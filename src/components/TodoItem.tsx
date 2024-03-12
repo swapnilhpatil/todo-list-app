b63a5a9abe5b63a8be6ee0dd1db58dae96654a03
Swapnil <swapnilhpatil9999@gmail.com>
2024-03-12 23:01:27 +0530

    updated todo item component with description

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -5,8 +5,10 @@ import React from 'react';
 const TodoItem : React.FC<TodoItemProps & TodoListActionProps> = ({ todo, onComplete, onDelete }) => {
   return (
     <div className="flex justify-between items-center border-b border-gray-200 py-2">
-      <span className={todo.completed ? 'line-through text-gray-400' : ''}>{todo.title}</span>
-      <span className={todo.completed ? 'line-through text-gray-400' : ''}>{todo.description}</span>
+      <div>
+        <h3 className={todo.completed ? 'line-through text-gray-400' : ''}>{todo.text}</h3>
+        <p>{todo.description}</p> {/* Render description */}
+      </div>
       <div>
         <button onClick={() => onComplete(todo.id)} className="text-green-500 mr-2">Complete</button>
         <button onClick={() => onDelete(todo.id)} className="text-red-500">Delete</button>
